d68d58d2049c358bf66ddc798f81ec9e6c88518a
Irene <irene.ng@berkeley.edu>
2023-07-01 14:52:13 +0700

    add clear button and random colors

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -13,16 +13,33 @@ for (let i=0; i< gridSize; i++) {
 
 const gridCells = document.querySelectorAll('.grid-cell');
 gridCells.forEach((cell) => {
+  let interact = 0;
   cell.addEventListener('mouseover', () => {
-    cell.classList.add('hovered');
+    // cell.classList.add('hovered');
+    let red = Math.floor(Math.random()*256);
+    let green = Math.floor(Math.random()*256);
+    let blue = Math.floor(Math.random() * 256);
+    let opacity = interact * 0.1;
+    cell.style.backgroundColor = `rgb(${red*(1-opacity)}, ${green*(1-opacity)}, ${blue*(1-opacity)})`;
+    interact++;
+    if (interact >=10) {
+      interact = 0;
+    }
   });
 });
 
-const sizeButton = document.querySelector("button#size-btn")
+const sizeButton = document.querySelector("button#size-btn");
 sizeButton.addEventListener('click', () => {
   gridSize = prompt('How many squares per row (max 100) do you want your grid to have?');
   gridCells.forEach((cell) => {
     cell.style.width = `${960 / gridSize}px`;
     cell.style.paddingBottom = `${960 / gridSize}px`;
+  })
+})
+
+const clearButton = document.querySelector("button#clear-btn");
+clearButton.addEventListener('click', () => {
+  gridCells.forEach((cell) => {
+    cell.style.backgroundColor = 'lightgrey';
   })
 })
